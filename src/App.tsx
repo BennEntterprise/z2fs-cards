347858c680d5520abe02bcfa79bfff01bdde8406
@BennEntterprise <kyle.bennett@betweenfiveandnine.com>
2021-09-22 22:38:23 -0400

    feat: clear out ui boilerplate from CRA!
    - throw a footer in there with a link to my github

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Counter from './components/Counter'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Counter />
       </header>
+      <main>
+        <Counter />
+      </main>
+      <footer id="footer"> This App Was Built By <a href="https://github.com/BennEntterprise">Kyle</a></footer>
     </div>
   );
 }
